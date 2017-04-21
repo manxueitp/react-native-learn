5c7a3556aefbf325d9bb4ae9caccaf420d11929b
manxueitp <mw3060@nyu.edu>
2017-04-21 16:50:56 -0700

    add new project album

---
+++ albums/index.ios.js
@@ -0,0 +1,14 @@
+//place code here for instructions
+
+//Import library
+import React from 'react';
+import {Text, AppRegistry} from 'react-native';
+
+//Create Component
+const App = () => (
+    <Text> hello </Text>
+);
+
+
+//Render it to the device
+AppRegistry.registerComponent('albums', () => App);
